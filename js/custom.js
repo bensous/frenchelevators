ee5e7d7e1ff7b0962ec981c69a05907aa471c793
C-A de Salaberry <cadesalaberry@yahoo.com>
2014-06-05 16:55:49 -0400

    Mov: js to custom.js.

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,27 @@
-$(document).ready(function() {
-  $("#splash").click(function() {
-   $("#splash").fadeOut(500);
+require.config({
+    paths: {
+        "BigVideo": "bower_components/BigVideo.js/lib/bigvideo",
+        "jquery": "bower_components/jquery/dist/jquery.min",
+        "jquery-ui": "bower_components/jquery-ui/ui/jquery-ui",
+        "videojs": "bower_components/video.js/dist/video-js/video",
+        "imagesloaded": "bower_components/imagesloaded/imagesloaded",
+        "eventEmitter/EventEmitter": "bower_components/eventEmitter/EventEmitter",
+        "eventie/eventie": "bower_components/eventie/eventie",
+        "modernizr": "bower_components/modernizr/modernizr"
+    },
+    shim: {
+        "videojs": {exports: 'videojs'}
+    }
+});
+
+require(['BigVideo'], function(bigvideo) {
+  $(function() {
+      var BV = new $.BigVideo({useFlashForFirefox:false});
+      BV.init();
+      BV.show('./videos/fecine-elevator-nothingloop.webmhd.webm',{altSource:'./videos/fecine-elevator-nothingloop.mp4',ambient:true});
   });
 });
+
+function do_nothing(){
+
+}
